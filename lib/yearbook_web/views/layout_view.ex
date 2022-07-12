fb7191f161d21c1fb333cac9a0be3b751988e06e
Nelson Estevão <nelsonmestevao@proton.me>
2022-07-12 01:11:39 +0100

    🐛 Fix when not logged in

--- lib/yearbook_web/views/layout_view.ex
+++ lib/yearbook_web/views/layout_view.ex
@@ -3,21 +3,29 @@ defmodule YearbookWeb.LayoutView do
 
   alias Yearbook.Uploaders
 
+  @conn YearbookWeb.Endpoint
+
   defguard is_logged_in(assigns) when not is_nil(assigns.current_user)
 
   def navbar(assigns) when is_logged_in(assigns) do
+    Enum.reduce(Enum.sort(assigns.current_user.permissions), [], fn role, acc ->
+      acc ++ navbar(role)
+    end)
+  end
+
+  def navbar(:admin) do
     [
       %{
         title: "Anos Letivos",
-        path: Routes.admin_academic_year_index_path(YearbookWeb.Endpoint, :index)
+        path: Routes.admin_academic_year_index_path(@conn, :index)
       },
       %{
         title: "Cursos",
-        path: Routes.admin_degree_index_path(YearbookWeb.Endpoint, :index)
+        path: Routes.admin_degree_index_path(@conn, :index)
       },
       %{
         title: "Turmas",
-        path: Routes.admin_class_index_path(YearbookWeb.Endpoint, :index)
+        path: Routes.admin_class_index_path(@conn, :index)
       }
     ]
   end
