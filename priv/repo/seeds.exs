4659a1688c36df2eb289d1b1bed948827b8d4c4b
Nuno Miguel <nmpf.2005@gmail.com>
2025-11-12 15:59:41 +0000

    chore: add seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,17 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Yearbook.Repo.insert!(%Yearbook.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Yearbook.Repo.Seeds do
+  @moduledoc """
+  Script for populating the database.
+  You can run it as:
+    $ mix run priv/repo/seeds.exs # or mix ecto.seed
+  """
+  @seeds_dir "priv/repo/seeds"
+
+  def run do
+    []
+    |> Enum.each(fn file ->
+      Code.require_file("#{@seeds_dir}/#{file}")
+    end)
+  end
+end
+
+Yearbook.Repo.Seeds.run()
