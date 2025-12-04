6a33964031d8a56368556f4f5cb0da336a1a373e
filipapeixoto25 <filipapeixoto700@proton.me>
2025-12-04 15:31:11 +0000

    feat: liveview page (#35)

---
+++ lib/yearbook_web/live/page_live.ex
@@ -0,0 +1,17 @@
+defmodule YearbookWeb.PageLive do
+  use YearbookWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div class="">
+      Hello World
+    </div>
+    """
+  end
+end
--- test/yearbook_web/controllers/page_controller_test.exs
+++ test/yearbook_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule YearbookWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Hello World"
   end
 end
