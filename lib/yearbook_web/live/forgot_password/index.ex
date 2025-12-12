80a190596193717d2e934513e08e27da72bd1eb1
PedroCoutinho <edr0c0de@protonmail.com>
2025-12-12 20:22:28 +0000

    fix:alias

--- lib/yearbook_web/live/forgot_password/index.ex
+++ lib/yearbook_web/live/forgot_password/index.ex
@@ -4,8 +4,6 @@ defmodule YearbookWeb.ForgotPassword.Index do
   """
   use YearbookWeb, :auth_view
 
-  alias Yearbook.Accounts
-
   def mount(_params, _session, socket) do
     socket = assign(socket, page_title: "Forgot Password | Yearbook")
     {:ok, socket}
