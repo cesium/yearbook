8f18b0a043a707905f139b09c64fe9399b30cdc1
Marcos Bernardo da Silva Lobo <marcosbernardolobo2006@gmail.com>
2025-12-17 14:23:59 +0000

    feat: configure gettext, add internationalization support for "Hello World", generate and update translation files

--- lib/yearbook_web/live/page_live.ex
+++ lib/yearbook_web/live/page_live.ex
@@ -1,15 +1,19 @@
 defmodule YearbookWeb.PageLive do
+  use Gettext, backend: YearbookWeb.Gettext
   use YearbookWeb, :live_view
 
   @impl true
   def mount(_params, _session, socket) do
+    # Gettext.put_locale(YearbookWeb.Gettext, "pt")
+    # mix gettext.extract
+    # mix gettext.merge priv/gettext
     {:ok, socket}
   end
 
   @impl true
   def render(assigns) do
     ~H"""
-    Hello World
+      {gettext("Hello World")}
     """
   end
 end
